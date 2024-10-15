bcc387734dace662c1372ea4ca53d8614bdb38f7
gkdudans <chrismhy001@gmail.com>
2024-10-16 01:19:28 +0900

    Feat: 카카오 로그인 구현

---
+++ core/src/main/java/org/bookwoori/core/domain/member/controller/AuthController.java
@@ -0,0 +1,34 @@
+package org.bookwoori.core.domain.member.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.bookwoori.core.domain.member.dto.LoginResponse;
+import org.bookwoori.core.domain.member.service.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@Tag(name = "Auth")
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+    private final MemberService memberService;
+
+    @GetMapping("/success")
+    public ResponseEntity<?> loginSuccess(@Valid LoginResponse loginResponse) {
+        return ResponseEntity.ok(loginResponse);
+    }
+
+    // 로그아웃
+
+    // 리프레시 토큰 재발급
+    
+}
---
+++ core/src/main/java/org/bookwoori/core/global/oauth/PrincipalDetails.java
@@ -0,0 +1,67 @@
+package org.bookwoori.core.global.oauth;
+
+import org.bookwoori.core.domain.member.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+public record PrincipalDetails (
+        Map<String, Object> attributes,
+        String attributeKey,
+        Member member) implements OAuth2User, UserDetails {
+
+    public Member getMember() {
+        return member;
+    }
+
+    @Override
+    public String getName() {
+        return attributes.get(attributeKey).toString();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;  // OAuth2 로그인에서는 패스워드를 사용하지 않음
+    }
+
+    @Override
+    public String getUsername() {
+        return String.valueOf(member.getKakaoId());
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
