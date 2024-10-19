0d2fcc6fd3ec10aca3c9c0b1344109a46ecfd495
gkdudans <chrismhy001@gmail.com>
2024-10-19 15:53:02 +0900

    Refactor: 하위 패키지로 파일 위치 이동

--- core/src/main/java/org/bookwoori/core/domain/member/controller/AuthController.java
+++ core/src/main/java/org/bookwoori/core/domain/member/controller/AuthController.java
@@ -1,16 +1,13 @@
 package org.bookwoori.core.domain.member.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bookwoori.core.domain.member.dto.LoginResponse;
+import org.bookwoori.core.domain.member.dto.response.LoginResponse;
 import org.bookwoori.core.domain.member.service.MemberService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
