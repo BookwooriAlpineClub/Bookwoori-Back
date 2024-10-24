da4dacb3d952f2b902539dc8bc35a657671fe2d0
Chaerin Hwang <chaa00@naver.com>
2024-10-25 03:06:03 +0900

    Feat: ENUM 역직렬화 관련 예외처리 코드 추가

--- core/src/main/java/org/bookwoori/core/domain/climbing/entity/ClimbingStatus.java
+++ core/src/main/java/org/bookwoori/core/domain/climbing/entity/ClimbingStatus.java
@@ -1,8 +1,22 @@
 package org.bookwoori.core.domain.climbing.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.bookwoori.core.domain.channel.entity.ChannelType;
+import org.bookwoori.core.global.exception.CustomException;
+import org.bookwoori.core.global.exception.ErrorCode;
+
 public enum ClimbingStatus {
     READY,
     RUNNING,
     FINISHED,
     FAILED;
+
+    @JsonCreator
+    public static ClimbingStatus from(String s) {
+        try {
+            return ClimbingStatus.valueOf(s.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new CustomException(ErrorCode.INVALID_ENUM_VALUE);
+        }
+    }
 }
