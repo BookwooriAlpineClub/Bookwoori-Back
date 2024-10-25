3e000d7a11f3eb90266b99ffd20af3bd65d39ecf
minseoooooo <oesnimnos@gmail.com>
2024-10-25 19:45:17 +0900

    Fix: 스프링시큐리티 기본 로그인화면 제거
    swagger이나 postman 테스트시 unauthorized 에러 발생해서 CoreApplication run시 자동으로 생기는 스프링시큐리티 기본 로그인화면 제거

--- core/src/main/java/org/bookwoori/core/CoreApplication.java
+++ core/src/main/java/org/bookwoori/core/CoreApplication.java
@@ -2,14 +2,15 @@ package org.bookwoori.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
 public class CoreApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(CoreApplication.class, args);
+  }
 
 }
