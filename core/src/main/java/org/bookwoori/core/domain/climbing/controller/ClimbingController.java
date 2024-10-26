6387829d81b1ea53bb59aec14f4e649031728c84
gkdudans <chrismhy001@gmail.com>
2024-10-27 03:53:24 +0900

    Fix: 클라이밍 채널 생성 시 상태 코드 201 반환하도록 수정

--- core/src/main/java/org/bookwoori/core/domain/climbing/controller/ClimbingController.java
+++ core/src/main/java/org/bookwoori/core/domain/climbing/controller/ClimbingController.java
@@ -4,13 +4,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.bookwoori.core.domain.channel.dto.request.ChannelCreateRequestDto;
 import org.bookwoori.core.domain.climbing.dto.request.ClimbingChannelCreateRequestDto;
 import org.bookwoori.core.domain.climbing.dto.request.ClimbingChannelUpdateRequestDto;
-import org.bookwoori.core.domain.climbing.entity.Climbing;
 import org.bookwoori.core.domain.climbing.facade.ClimbingFacade;
-import org.bookwoori.core.domain.climbing.service.ClimbingService;
-import org.bookwoori.core.domain.member.dto.request.UpdateMemberRequestDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +22,7 @@ public class ClimbingController {
     @PostMapping
     public ResponseEntity<?> createClimbingChannel(@RequestBody @Valid ClimbingChannelCreateRequestDto requestDto) {
         climbingFacade.createClimbing(requestDto);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @Operation(summary = "클라이밍 채널 편집", description = "클라이밍 채널을 편집합니다.")
