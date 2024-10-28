1d28c9d419b318b722847e333b2dcc12dd5dcb5f
minseoooooo <oesnimnos@gmail.com>
2024-10-28 20:16:14 +0900

    Style: 코드 포맷팅 (indent)
    기존 커밋에 미적용 되었던 탭 크기, 들여쓰기 적용

--- core/src/main/java/org/bookwoori/core/domain/book/service/BookService.java
+++ core/src/main/java/org/bookwoori/core/domain/book/service/BookService.java
@@ -16,87 +16,87 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class BookService {
 
-  private final BookRepository bookRepository;
+    private static final String ALADIN_API_URL_SEARCH = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx";
+    private static final String ALADIN_API_URL_LOOKUP = "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx";
+    private static final String TTB_KEY = "ttboesnimnos1216001"; // 알라딘 API Key
+    private final BookRepository bookRepository;
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-  private static final String ALADIN_API_URL_SEARCH = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx";
-  private static final String ALADIN_API_URL_LOOKUP = "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx";
-  private static final String TTB_KEY = "ttboesnimnos1216001"; // 알라딘 API Key
+    public List<BookResponseDto> findBooksByKeyword(String keyword) { // 도서 검색
+        String url = ALADIN_API_URL_SEARCH + "?ttbkey=" + TTB_KEY +
+            "&Query=" + keyword +
+            "&QueryType=Keyword" +
+            "&MaxResults=100" +
+            "&start=1" +
+            "&SearchTarget=Book" +
+            "&output=js" +
+            "&Version=20131101";
 
-  @Autowired
-  private RestTemplate restTemplate;
-  @Autowired
-  private ObjectMapper objectMapper;
+        String jsonResponse = restTemplate.getForObject(url, String.class);
 
-  public List<BookResponseDto> findBooksByKeyword(String keyword) { // 도서 검색
-    String url = ALADIN_API_URL_SEARCH + "?ttbkey=" + TTB_KEY +
-        "&Query=" + keyword +
-        "&QueryType=Keyword" +
-        "&MaxResults=100" +
-        "&start=1" +
-        "&SearchTarget=Book" +
-        "&output=js" +
-        "&Version=20131101";
+        List<BookResponseDto> bookList = new ArrayList<>();
 
-    String jsonResponse = restTemplate.getForObject(url, String.class);
+        try {
+            JsonNode root = objectMapper.readTree(jsonResponse);
+            JsonNode items = root.path("item");
 
-    List<BookResponseDto> bookList = new ArrayList<>();
-
-    try {
-      JsonNode root = objectMapper.readTree(jsonResponse);
-      JsonNode items = root.path("item");
-
-      if (items.isArray()) {
-        for (JsonNode item : items) {
-          String title = item.path("title").asText();
-          String author = item.path("author").asText();
-          String publisher = item.path("publisher").asText();
-          String pubYear = item.path("pubDate").asText().split("-")[0];
-          String isbn13 = item.path("isbn13").asText();
-          String cover = item.path("cover").asText();
-          BookResponseDto bookResponseDto = new BookResponseDto(title, author, publisher, pubYear,
-              isbn13, cover);
-          bookList.add(bookResponseDto);
+            if (items.isArray()) {
+                for (JsonNode item : items) {
+                    String title = item.path("title").asText();
+                    String author = item.path("author").asText();
+                    String publisher = item.path("publisher").asText();
+                    String pubYear = item.path("pubDate").asText().split("-")[0];
+                    String isbn13 = item.path("isbn13").asText();
+                    String cover = item.path("cover").asText();
+                    BookResponseDto bookResponseDto = new BookResponseDto(title, author, publisher,
+                        pubYear,
+                        isbn13, cover);
+                    bookList.add(bookResponseDto);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+
+        return bookList;
     }
 
-    return bookList;
-  }
 
+    public BookDetailResponseDto findBookByIsbn(String isbn13) { // 상세정보 조회
+        String url = ALADIN_API_URL_LOOKUP + "?ttbkey=" + TTB_KEY +
+            "&itemIdType=ISBN13" +
+            "&ItemId=" + isbn13 +
+            "&output=js" +
+            "&Version=20131101";
 
-  public BookDetailResponseDto findBookByIsbn(String isbn13) { // 상세정보 조회
-    String url = ALADIN_API_URL_LOOKUP + "?ttbkey=" + TTB_KEY +
-        "&itemIdType=ISBN13" +
-        "&ItemId=" + isbn13 +
-        "&output=js" +
-        "&Version=20131101";
+        String jsonResponse = restTemplate.getForObject(url, String.class);
 
-    String jsonResponse = restTemplate.getForObject(url, String.class);
+        try {
+            JsonNode root = objectMapper.readTree(jsonResponse);
+            JsonNode item = root.path("item").get(0);
 
-    try {
-      JsonNode root = objectMapper.readTree(jsonResponse);
-      JsonNode item = root.path("item").get(0);
+            if (item != null) {
+                String title = item.path("title").asText();
+                String author = item.path("author").asText();
+                String publisher = item.path("publisher").asText();
+                String pubDate = item.path("pubDate").asText();
+                Long itemPage = item.path("subInfo").path("itemPage").asLong();
+                String description = item.path("description").asText();
+                String cover = item.path("cover").asText();
 
-      if (item != null) {
-        String title = item.path("title").asText();
-        String author = item.path("author").asText();
-        String publisher = item.path("publisher").asText();
-        String pubDate = item.path("pubDate").asText();
-        Long itemPage = item.path("subInfo").path("itemPage").asLong();
-        String description = item.path("description").asText();
-        String cover = item.path("cover").asText();
+                return new BookDetailResponseDto(title, author, publisher, pubDate, itemPage,
+                    description,
+                    isbn13, cover);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        return new BookDetailResponseDto(title, author, publisher, pubDate, itemPage, description,
-            isbn13, cover);
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+        return null; // 데이터가 없거나 오류 발생 시 null 반환
     }
-
-    return null; // 데이터가 없거나 오류 발생 시 null 반환
-  }
 
 
 }
