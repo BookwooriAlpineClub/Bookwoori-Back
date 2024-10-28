a8982d25e93e5ce0f32a7a35e3a8037b8ed87c27
gkdudans <chrismhy001@gmail.com>
2024-10-28 19:17:00 +0900

    Fix: 닉네임 글자수 제한 추가

--- core/src/main/java/org/bookwoori/core/domain/member/dto/request/UpdateMemberRequestDto.java
+++ core/src/main/java/org/bookwoori/core/domain/member/dto/request/UpdateMemberRequestDto.java
@@ -1,10 +1,13 @@
 package org.bookwoori.core.domain.member.dto.request;
 
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
 
 public record UpdateMemberRequestDto(
-        @NotNull String nickname,
+        @Size(max= 10, message = "INVALID_INPUT_LENGTH-닉네임은 10자 이내여야 합니다.")
+        @NotBlank String nickname,
         @Nullable MultipartFile profileImg) {
 }
