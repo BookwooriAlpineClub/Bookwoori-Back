7ddd623ff62a392fe04a0ac2e7b7e40df5571390
minseoooooo <oesnimnos@gmail.com>
2024-10-28 20:50:28 +0900

    Fix: exclude = SecurityAutoConfiguration.class 코드 제거

--- core/src/main/java/org/bookwoori/core/CoreApplication.java
+++ core/src/main/java/org/bookwoori/core/CoreApplication.java
@@ -2,15 +2,14 @@ package org.bookwoori.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 @EnableJpaAuditing
 public class CoreApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(CoreApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
 
 }
