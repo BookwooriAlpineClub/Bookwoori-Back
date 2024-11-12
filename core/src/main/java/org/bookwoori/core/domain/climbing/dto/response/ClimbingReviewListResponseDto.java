92de9044d33de114ce7471459bbc3d0dcc269652
gkdudans <chrismhy001@gmail.com>
2024-11-12 17:36:32 +0900

    Feat: 감상평 공유 가능 여부를 조회할 수 있는 API로 구현

--- core/src/main/java/org/bookwoori/core/domain/climbing/dto/response/ClimbingReviewListResponseDto.java
+++ core/src/main/java/org/bookwoori/core/domain/climbing/dto/response/ClimbingReviewListResponseDto.java
@@ -3,6 +3,7 @@ package org.bookwoori.core.domain.climbing.dto.response;
 import java.util.List;
 
 public record ClimbingReviewListResponseDto(
+    boolean hasShared,
     List<ClimbingMemberReviewUnitDto> ClimbingMemberReviewList
 ) {
 
--- core/src/main/java/org/bookwoori/core/domain/climbing/facade/ClimbingMemberFacade.java
+++ core/src/main/java/org/bookwoori/core/domain/climbing/facade/ClimbingMemberFacade.java
@@ -13,6 +13,8 @@ import org.bookwoori.core.domain.climbing.dto.response.ClimbingMemberResponseDto
 import org.bookwoori.core.domain.climbing.dto.response.ClimbingMemberReviewUnitDto;
 import org.bookwoori.core.domain.climbing.dto.response.ClimbingMemberUnitDto;
 import org.bookwoori.core.domain.climbing.dto.response.ClimbingReviewListResponseDto;
+import org.bookwoori.core.domain.climbing.dto.response.ClimbingReviewWithShareResponseDto;
+import org.bookwoori.core.domain.climbing.dto.response.ClimbingReviewWithoutShareResponseDto;
 import org.bookwoori.core.domain.climbing.dto.response.ReviewEmojiListCountDto;
 import org.bookwoori.core.domain.climbing.dto.response.ReviewEmojiListDto;
 import org.bookwoori.core.domain.climbing.dto.response.ReviewEmojiMemberListResponseDto;
@@ -109,6 +111,21 @@ public class ClimbingMemberFacade {
     }
 
     @Transactional(readOnly = true)
+    public boolean isHasShared(Long climbingId) {
+        Member currentMember = memberService.getCurrentMember();
+        ClimbingMember climbingMember = climbingMemberService.getMemberInClimbing(currentMember,
+            climbingId);
+        return climbingMember.isHasShared();
+    }
+
+    @Transactional(readOnly = true)
+    public boolean isAllowsShare(Long climbingId) {
+        Member currentMember = memberService.getCurrentMember();
+        Climbing climbing = climbingService.getClimbingById(climbingId);
+        return reviewService.existsReviewByMemberAndBook(currentMember, climbing.getBook());
+    }
+
+    @Transactional(readOnly = true)
     public ClimbingReviewListResponseDto getClimbingReviewList(Long climbingId) {
         Climbing climbing = climbingService.getClimbingById(climbingId);
         // sharedReviews: hasShared true인 ClimbingMember의 Review 리스트
@@ -151,7 +168,21 @@ public class ClimbingMemberFacade {
                 return ClimbingMemberReviewUnitDto.from(climbingMember, review, reviewEmojiList);
             })
             .collect(Collectors.toList());
-        return new ClimbingReviewListResponseDto(climbingReviews);
+        return new ClimbingReviewListResponseDto(true, climbingReviews);
+    }
+
+    @Transactional(readOnly = true)
+    public ClimbingReviewWithShareResponseDto getReviewWithAllowShare(Long climbingId) {
+        Member currentMember = memberService.getCurrentMember();
+        Climbing climbing = climbingService.getClimbingById(climbingId);
+        Review review = reviewService.getReviewByMemberAndBook(currentMember, climbing.getBook());
+        return ClimbingReviewWithShareResponseDto.from(climbing, review);
+    }
+
+    @Transactional(readOnly = true)
+    public ClimbingReviewWithoutShareResponseDto getReviewWithoutAllowShare(Long climbingId) {
+        Climbing climbing = climbingService.getClimbingById(climbingId);
+        return ClimbingReviewWithoutShareResponseDto.from(climbing);
     }
 
     public boolean toggleReviewReaction(Long climbingId, Long reviewId, EmojiType emoji) {
@@ -196,6 +227,5 @@ public class ClimbingMemberFacade {
             .collect(Collectors.toList());
         return new ReviewEmojiMemberListResponseDto(emojiLists);
     }
-
 
 }
