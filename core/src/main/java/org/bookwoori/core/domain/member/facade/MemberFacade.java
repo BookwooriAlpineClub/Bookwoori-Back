088bc7188031727a626cb1c87dc74bbe082aa5d7
gkdudans <chrismhy001@gmail.com>
2024-11-12 14:58:30 +0900

    Feat: 유저 서재 조회 API 구현

--- core/src/main/java/org/bookwoori/core/domain/member/facade/MemberFacade.java
+++ core/src/main/java/org/bookwoori/core/domain/member/facade/MemberFacade.java
@@ -3,14 +3,17 @@ package org.bookwoori.core.domain.member.facade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bookwoori.core.domain.member.dto.request.UpdateMemberRequestDto;
+import org.bookwoori.core.domain.member.dto.response.MemberLibraryResponseDto;
 import org.bookwoori.core.domain.member.entity.Member;
 import org.bookwoori.core.domain.member.service.MemberService;
 import org.bookwoori.core.global.s3.S3Util;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class MemberFacade {
 
     private final MemberService memberService;
@@ -21,5 +24,13 @@ public class MemberFacade {
         String profileImgUrl = s3Util.uploadImage(requestDto.profileImg(), "member/profile-image");
         currentMember.updateMember(requestDto.nickname(), profileImgUrl);
         memberService.saveMember(currentMember);
+    }
+
+    @Transactional(readOnly = true)
+    public MemberLibraryResponseDto getMemberLibrary(Long memberId) {
+        Member member = memberService.getMemberById(memberId);
+        Member currentMember = memberService.getCurrentMember();
+        boolean isMine = member.equals(currentMember);
+        return MemberLibraryResponseDto.from(member, isMine);
     }
 }
