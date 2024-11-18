268a7dcd037c9b1c489718ffac433bbbe090d303
gkdudans <chrismhy001@gmail.com>
2024-11-19 02:38:15 +0900

    fix: Swagger CORS 문제 해결

--- core/src/main/java/org/bookwoori/core/CoreApplication.java
+++ core/src/main/java/org/bookwoori/core/CoreApplication.java
@@ -1,5 +1,7 @@
 package org.bookwoori.core;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +10,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "https://api.bookwoori.p-e.kr", description = "Default Server URL")
+    }
+)
 public class CoreApplication {
 
     public static void main(String[] args) {
