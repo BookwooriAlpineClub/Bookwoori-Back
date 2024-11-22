b70d80b8f634f0b545d187164c67e1a9840c563f
gkdudans <chrismhy001@gmail.com>
2024-11-22 14:04:37 +0900

    fix: 쿠키 헤더 설정 코드 수정

--- core/src/main/java/org/bookwoori/core/global/jwt/CookieUtil.java
+++ core/src/main/java/org/bookwoori/core/global/jwt/CookieUtil.java
@@ -11,21 +11,29 @@ public class CookieUtil {
 
     public static final int REFRESH_TOKEN_MAX_AGE = 7 * 24 * 60 * 60;  // 7일
 
+//    public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+//        Cookie cookie = new Cookie(name, value);
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(true);
+//        cookie.setPath("/");
+//        cookie.setMaxAge(maxAge);
+//        response.addCookie(cookie);
+//
+//        // SameSite 설정 추가
+//        String sameSiteValue = "None";
+//        response.addHeader("Set-Cookie",
+//            String.format("%s=%s; Max-Age=%d; Path=/; HttpOnly; SameSite=None",
+//                name, value, maxAge, sameSiteValue, cookie.getSecure() ? "; Secure" : ""));
+//    }
+
     public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(maxAge);
-        response.addCookie(cookie);
-
-        // SameSite 설정 추가
-        String sameSiteValue = "None";
-        response.addHeader("Set-Cookie",
-            String.format("%s=%s; Max-Age=%d; Path=/; HttpOnly; SameSite=%s%s",
-                name, value, maxAge, sameSiteValue, cookie.getSecure() ? "; Secure" : ""));
+        String cookie = String.format(
+            "%s=%s; Max-Age=%d; Path=/; HttpOnly; SameSite=None; Secure",
+            name, value, maxAge
+        );
+        response.addHeader("Set-Cookie", cookie);
     }
-
+    
     public Cookie getCookie(HttpServletRequest request, String name) {
         if (request.getCookies() != null) {
             return Arrays.stream(request.getCookies())
