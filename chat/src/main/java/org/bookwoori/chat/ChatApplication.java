6ed1ebc82be76d314e9a906fd91aa9e98c5f9df9
gkdudans <chrismhy001@gmail.com>
2024-11-24 21:33:50 +0900

    Feat: Chat 서버 gateway 설정 추가

--- chat/src/main/java/org/bookwoori/chat/ChatApplication.java
+++ chat/src/main/java/org/bookwoori/chat/ChatApplication.java
@@ -1,5 +1,7 @@
 package org.bookwoori.chat;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +10,11 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableMongoRepositories
 @EnableFeignClients
+@OpenAPIDefinition(servers = {
+    @Server(url = "http://localhost:8000", description = "Local Gateway Server"),
+    @Server(url = "http://localhost:8081", description = "Local Development Server"),
+    @Server(url = "https://api.bookwoori.p-e.kr", description = "Production Server")
+})
 public class ChatApplication {
 
     public static void main(String[] args) {
