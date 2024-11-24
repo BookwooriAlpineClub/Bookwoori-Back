61566ccadbfffae816c8f7499d30f51591d51d71
gkdudans <chrismhy001@gmail.com>
2024-11-24 22:15:03 +0900

    Fix: SecurityConfig 코드 수정

--- chat/src/main/java/org/bookwoori/chat/global/config/SecurityConfig.java
+++ chat/src/main/java/org/bookwoori/chat/global/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
             "http://localhost:3000",
             "http://localhost:8080",
             "http://localhost:8001",
+            "http://localhost:8000",
             "https://api.bookwoori.p-e.kr"));
         configuration.setAllowedMethods(
             Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"));
