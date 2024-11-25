f08d473f69a81d0ea75646acfd6f1d7831432135
Chaerin Hwang <chaa00@naver.com>
2024-11-25 13:04:14 +0900

    Feat: 에러처리 코드 추가

--- chat/src/main/java/org/bookwoori/chat/global/EmojiType.java
+++ chat/src/main/java/org/bookwoori/chat/global/EmojiType.java
@@ -4,14 +4,13 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import org.bookwoori.chat.global.exception.CustomException;
 import org.bookwoori.chat.global.exception.ErrorCode;
 
-public enum MessageType {
-    TALK, FILE;
-
+public enum EmojiType {
+    THUMBS_UP, HEART_HANDS, SMILING_FACE, CRYING_FACE, THINKING_FACE;
 
     @JsonCreator
-    public static MessageType from(String s) {
+    public static EmojiType from(String s) {
         try {
-            return MessageType.valueOf(s.toUpperCase());
+            return EmojiType.valueOf(s.toUpperCase());
         } catch (IllegalArgumentException e) {
             throw new CustomException(ErrorCode.INVALID_ENUM_VALUE);
         }
