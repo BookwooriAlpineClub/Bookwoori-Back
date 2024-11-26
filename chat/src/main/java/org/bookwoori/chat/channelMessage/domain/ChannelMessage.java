27ef693e870622d68e67fabbf3b16881d576caef
Chaerin Hwang <chaa00@naver.com>
2024-11-27 04:23:30 +0900

    Feat: 채널 메시지 엔티티 수정
    반응/답장 관련 필드 추가

--- chat/src/main/java/org/bookwoori/chat/channelMessage/domain/ChannelMessage.java
+++ chat/src/main/java/org/bookwoori/chat/channelMessage/domain/ChannelMessage.java
@@ -1,13 +1,20 @@
 package org.bookwoori.chat.channelMessage.domain;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.bookwoori.chat.global.common.EmojiType;
+import org.bookwoori.chat.global.common.EventType;
 import org.bookwoori.chat.global.common.MessageType;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "channelMessage")
@@ -30,15 +37,41 @@ public class ChannelMessage {
 
     private LocalDateTime createdAt;
 
-    @Override
-    public String toString() {
-        return "ChannelMessage{" +
-            "id='" + id + '\'' +
-            ", channelId=" + channelId +
-            ", memberId=" + memberId +
-            ", type='" + type + '\'' +
-            ", content='" + content + '\'' +
-            ", createdAt=" + createdAt +
-            '}';
+    private Map<EmojiType, Set<Long>> reactions = new HashMap<>();
+
+    private String parentId;
+
+    /*
+     * 데이터베이스에 저장되지 않는 필드
+     */
+    @Transient
+    private String parentContent;
+
+    @Transient
+    private EventType eventType;
+
+    @Transient
+    private EmojiType targetEmoji;
+
+    public void addReaction(EmojiType emoji, Long memberId) {
+        reactions.putIfAbsent(emoji, new HashSet<>());
+        reactions.get(emoji).add(memberId);
+    }
+
+    public void removeReaction(EmojiType emoji, Long memberId) {
+        if (reactions.containsKey(emoji)) {
+            reactions.get(emoji).remove(memberId);
+            if (reactions.get(emoji).isEmpty()) {
+                reactions.remove(emoji);
+            }
+        }
+    }
+
+    public void setEventType(EventType eventType) {
+        this.eventType = eventType;
+    }
+
+    public void setTargetEmoji(EmojiType emoji) {
+        this.targetEmoji = emoji;
     }
 }
