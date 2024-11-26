0b69d605eff15327898cbc4e8e9dfedc30127ff6
minseoooooo <oesnimnos@gmail.com>
2024-11-26 22:25:22 +0900

    Refactor: record 기능 에러 해결 / 리팩토링

--- core/src/main/java/org/bookwoori/core/domain/review/repository/ReviewRepository.java
+++ core/src/main/java/org/bookwoori/core/domain/review/repository/ReviewRepository.java
@@ -1,6 +1,5 @@
 package org.bookwoori.core.domain.review.repository;
 
-import java.util.List;
 import org.bookwoori.core.domain.book.entity.Book;
 import org.bookwoori.core.domain.member.entity.Member;
 import org.bookwoori.core.domain.review.entity.Review;
@@ -8,18 +7,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("SELECT r FROM Review r WHERE r.record.member.id IN :memberIds AND r.record.book = :book")
     List<Review> findByMemberIdsAndBook(@Param("memberIds") List<Long> memberIds,
-        @Param("book") Book book);
+                                        @Param("book") Book book);
 
-    boolean existsByMemberAndBook(Member member, Book book);
+    boolean existsByRecord_MemberAndRecord_Book(Member member, Book book);
 
     @Query("SELECT r FROM Review r WHERE r.record.member = :member AND r.record.book = :book")
     Review findByMemberAndBook(@Param("member") Member member, @Param("book") Book book);
 
-    Review findByRecord_RecordId(Long recordId);
+    Optional<Review> findByRecord_RecordId(Long recordId);
 
     void deleteByRecord_RecordId(Long recordId);
 
