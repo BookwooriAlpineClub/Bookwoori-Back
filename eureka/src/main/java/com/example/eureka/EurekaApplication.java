46322a7883f7b1799dc962ca4c13e5bc30cdbff1
gkdudans <chrismhy001@gmail.com>
2024-11-27 18:03:31 +0900

    Chore: 2차 배포 준비

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -1,15 +1,15 @@
-package com.example.discoveryservice;
+package com.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer //
-public class DiscoveryserviceApplication {
+@EnableEurekaServer
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryserviceApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 
 }
