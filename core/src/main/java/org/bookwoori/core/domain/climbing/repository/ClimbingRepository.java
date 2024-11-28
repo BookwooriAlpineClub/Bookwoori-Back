7c8d7f0937547433dd0694f8c797c88ce1cc3e90
gkdudans <chrismhy001@gmail.com>
2024-11-29 02:52:37 +0900

    Feat: 클라이밍 목록 조회 추가 구현

--- core/src/main/java/org/bookwoori/core/domain/climbing/repository/ClimbingRepository.java
+++ core/src/main/java/org/bookwoori/core/domain/climbing/repository/ClimbingRepository.java
@@ -2,6 +2,7 @@ package org.bookwoori.core.domain.climbing.repository;
 
 import java.util.List;
 import org.bookwoori.core.domain.climbing.entity.Climbing;
+import org.bookwoori.core.domain.climbing.entity.ClimbingStatus;
 import org.bookwoori.core.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,8 @@ public interface ClimbingRepository extends JpaRepository<Climbing, Long> {
 
     @Query("SELECT c FROM Climbing c WHERE c.status = 'READY' AND c.server.serverId = :serverId ORDER BY c.startDate ASC, c.climbingId DESC")
     List<Climbing> findReadyClimbings(Long serverId);
+
+    List<Climbing> findByServer_ServerIdAndStatus(Long serverId, ClimbingStatus climbingStatus);
+
+    List<Climbing> findByServer_ServerIdAndStatusIn(Long serverId, List<ClimbingStatus> statuses);
 }
