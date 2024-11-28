9ce90b43cc8cf5615aa9846a3be91ca26e5a8b4d
gkdudans <chrismhy001@gmail.com>
2024-11-28 09:55:14 +0900

    Chore: 로그인 성공 URL 수정

--- auth/src/main/java/org/bookwoori/auth/global/handler/OAuth2SuccessHandler.java
+++ auth/src/main/java/org/bookwoori/auth/global/handler/OAuth2SuccessHandler.java
@@ -19,7 +19,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     private final TokenProvider tokenProvider;
     private final CookieUtil cookieUtil;
-    private static final String SUCCESS_URI = "https://d1b8jphm7kfx0c.cloudfront.net/auth/success";
+    private static final String SUCCESS_URI = "https://api.bookwoori.p-e.kr//auth/success";
     private static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
 
     @Override
