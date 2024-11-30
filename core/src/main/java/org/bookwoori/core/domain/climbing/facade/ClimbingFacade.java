a4f6cbb5f0ff9ae715d26e9d0f624a4494bd5053
gkdudans <chrismhy001@gmail.com>
2024-11-30 14:41:25 +0900

    Feat: 클라이밍 채널 API 권한 관리

--- core/src/main/java/org/bookwoori/core/domain/climbing/facade/ClimbingFacade.java
+++ core/src/main/java/org/bookwoori/core/domain/climbing/facade/ClimbingFacade.java
@@ -24,6 +24,7 @@ import org.bookwoori.core.domain.record.entity.ReadingStatus;
 import org.bookwoori.core.domain.record.service.RecordService;
 import org.bookwoori.core.domain.server.entity.Server;
 import org.bookwoori.core.domain.server.service.ServerService;
+import org.bookwoori.core.domain.serverMember.service.ServerMemberService;
 import org.bookwoori.core.global.exception.CustomException;
 import org.bookwoori.core.global.exception.ErrorCode;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -41,6 +42,7 @@ public class ClimbingFacade {
     private final BookService bookService;
     private final ClimbingMemberService climbingMemberService;
     private final RecordService recordService;
+    private final ServerMemberService serverMemberService;
 
     @Scheduled(cron = "0 0 0 * * *")
     public void updateClimbingStatus() {
@@ -97,6 +99,9 @@ public class ClimbingFacade {
     public ClimbingDetailsResponseDto getClimbingDetails(Long climbingId) {
         Member currentMember = memberService.getCurrentMember();
         Climbing climbing = climbingService.getClimbingById(climbingId);
+        if (!serverMemberService.isJoined(currentMember, climbing.getServer())) {
+            throw new CustomException(ErrorCode.ACCESS_DENIED);
+        }
         int memberCount = climbingMemberService.getMemberCount(climbing);
         boolean isJoined = climbingMemberService.isJoined(currentMember, climbing);
         boolean isOwner = climbingMemberService.isOwner(currentMember, climbing);
@@ -106,6 +111,10 @@ public class ClimbingFacade {
     @Transactional(readOnly = true)
     public ServerClimbingListDto getClimbingList(Long serverId) {
         Member currentMember = memberService.getCurrentMember();
+        Server server = serverService.getServerById(serverId);
+        if (!serverMemberService.isJoined(currentMember, server)) {
+            throw new CustomException(ErrorCode.ACCESS_DENIED);
+        }
         // myClimbs
         List<ServerClimbingListDto.ClimbingUnitDto> myClimbings = climbingService.getMyClimbings(
                 currentMember, serverId).stream()
@@ -138,6 +147,10 @@ public class ClimbingFacade {
     @Transactional(readOnly = true)
     public MyClimbingListResponseDto getMyClimbingList(Long serverId) {
         Member currentMember = memberService.getCurrentMember();
+        Server server = serverService.getServerById(serverId);
+        if (!serverMemberService.isJoined(currentMember, server)) {
+            throw new CustomException(ErrorCode.ACCESS_DENIED);
+        }
         List<Climbing> myClimbings = climbingService.getMyClimbings(currentMember, serverId);
         List<ClimbingDetailsResponseDto> myClimbingList = convertToDto(myClimbings);
         return new MyClimbingListResponseDto(myClimbingList);
@@ -145,6 +158,11 @@ public class ClimbingFacade {
 
     @Transactional(readOnly = true)
     public ReadyClimbingListResponseDto getReadyClimbingList(Long serverId) {
+        Member currentMember = memberService.getCurrentMember();
+        Server server = serverService.getServerById(serverId);
+        if (!serverMemberService.isJoined(currentMember, server)) {
+            throw new CustomException(ErrorCode.ACCESS_DENIED);
+        }
         List<Climbing> readyClimbings = climbingService.getReadyClimbings(serverId);
         List<ClimbingDetailsResponseDto> readyClimbingList = convertToDto(readyClimbings);
         return new ReadyClimbingListResponseDto(readyClimbingList);
