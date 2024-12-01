47910e050e5fea115868839634f96d4b97528598
gkdudans <chrismhy001@gmail.com>
2024-12-02 00:16:23 +0900

    Chore: 로그인 성공 리다이렉트 URI 수정

--- auth/src/main/java/org/bookwoori/auth/global/handler/OAuth2SuccessHandler.java
+++ auth/src/main/java/org/bookwoori/auth/global/handler/OAuth2SuccessHandler.java
@@ -19,7 +19,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     private final TokenProvider tokenProvider;
     private final CookieUtil cookieUtil;
-    private static final String SUCCESS_URI = "http://localhost/auth/success";
+    private static final String SUCCESS_URI = "http://localhost:3000/auth/success";
     private static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
 
     @Override
