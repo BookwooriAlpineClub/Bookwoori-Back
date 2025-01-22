e880fb857d29af0cea69186ecb5c56d78a9841a6
gkdudans <chrismhy001@gmail.com>
2025-01-22 15:38:19 +0900

    Style: 사용하지 않는 코드 삭제

--- core/src/main/java/org/bookwoori/core/domain/member/entity/Member.java
+++ core/src/main/java/org/bookwoori/core/domain/member/entity/Member.java
@@ -76,13 +76,13 @@ public class Member extends BaseTimeEntity {
         this.nickname = nickname;
     }
 
-    public void updateHeight(double xp) {
-        this.grade.height += xp;
-        Grade newGrade = Grade.findGradeByHeight(this.grade.height);
-        if (!this.grade.equals(newGrade)) {
-            this.grade = newGrade;
-        }
-    }
+//    public void updateHeight(double xp) {
+//        this.grade.height += xp;
+//        Grade newGrade = Grade.findGradeByHeight(this.grade.height);
+//        if (!this.grade.equals(newGrade)) {
+//            this.grade = newGrade;
+//        }
+//    }
 
     public void updateMemberProfileImg(String url) {
         this.profileImg = url;
--- core/src/main/java/org/bookwoori/core/global/s3/S3Util.java
+++ core/src/main/java/org/bookwoori/core/global/s3/S3Util.java
@@ -82,25 +82,6 @@ public class S3Util {
         }
     }
 
-    public boolean isSameImage(MultipartFile newImage, String oldImageUrl) {
-        if (newImage == null || oldImageUrl == null || oldImageUrl.isEmpty()) {
-            return false;
-        }
-        try (InputStream inputStream = newImage.getInputStream()) {
-            String delimiter = ".com/";
-            int index = oldImageUrl.lastIndexOf(delimiter);
-            if (index == -1) {
-                return false;
-            }
-            String fileName = oldImageUrl.substring(index + delimiter.length());
-            InputStream existingImageStream = amazonS3.getObject(bucket, fileName)
-                .getObjectContent();
-            return compareStreams(inputStream, existingImageStream);
-        } catch (IOException e) {
-            throw new CustomException(ErrorCode.FILE_COMPARISON_FAIL);
-        }
-    }
-
     private boolean compareStreams(InputStream stream1, InputStream stream2) throws IOException {
         int byte1, byte2;
         while ((byte1 = stream1.read()) != -1 && (byte2 = stream2.read()) != -1) {
