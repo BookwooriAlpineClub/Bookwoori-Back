95d21a2709e1fccff2d5c018bb870224e2435799
gkdudans <chrismhy001@gmail.com>
2025-01-22 16:08:23 +0900

    Refactor: 사용하지 않는 코드 정리, 어노테이션 추가

--- core/src/main/java/org/bookwoori/core/domain/member/dto/request/UpdateMemberProfilImgRequestDto.java
+++
@@ -1,9 +0,0 @@
-package org.bookwoori.core.domain.member.dto.request;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public record UpdateMemberProfilImgRequestDto(
-    MultipartFile profileImg
-) {
-
-}
