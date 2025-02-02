4f6813ba696a3c5c0317d59ca0eb8d4c1a9f5d91
gkdudans <chrismhy001@gmail.com>
2025-02-03 02:32:08 +0900

    Feat: 책 정보 조회 응답 수정
    pubDate로 통일, coverImg → cover로 수정

--- core/src/main/java/org/bookwoori/core/domain/book/dto/response/BookDetailsResponseDto.java
+++ core/src/main/java/org/bookwoori/core/domain/book/dto/response/BookDetailsResponseDto.java
@@ -10,22 +10,22 @@ public record BookDetailsResponseDto(
     String title,
     String author,
     String publisher,
-    String pubDate,
+    LocalDate pubDate,
     Long itemPage,
     String description,
     String isbn13,
-    String coverImg) {
+    String cover) {
 
     public static BookDetailsResponseDto from(JsonNode item) {
         return BookDetailsResponseDto.builder()
             .title(item.path("title").asText())
             .author(item.path("author").asText())
             .publisher(item.path("publisher").asText())
-            .pubDate(item.path("pubDate").asText())
+            .pubDate(LocalDate.parse(item.path("pubDate").asText()))
             .itemPage(item.path("subInfo").path("itemPage").asLong())
             .description(item.path("description").asText())
             .isbn13(item.path("isbn13").asText())
-            .coverImg(item.path("cover").asText())
+            .cover(item.path("cover").asText())
             .build();
     }
 
@@ -34,11 +34,11 @@ public record BookDetailsResponseDto(
             .title(this.title)
             .author(this.author)
             .publisher(this.publisher)
-            .pubDate(LocalDate.parse(this.pubDate))
+            .pubDate(this.pubDate)
             .itemPage(this.itemPage != null ? Math.toIntExact(this.itemPage) : null)
             .isbn13(this.isbn13)
             .description(this.description)
-            .coverImg(this.coverImg)
+            .coverImg(this.cover)
             .build();
     }
 }
