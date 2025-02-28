34da598416c51260b1808761eff0dea8d286eabc
gkdudans <chrismhy001@gmail.com>
2025-02-28 15:21:16 +0900

    Fix: 책 기록 상세 조회 API 수정

--- core/src/main/java/org/bookwoori/core/domain/record/facade/RecordFacade.java
+++ core/src/main/java/org/bookwoori/core/domain/record/facade/RecordFacade.java
@@ -91,7 +91,6 @@ public class RecordFacade {
             .collect(Collectors.toList());
     }
 
-    @Transactional(readOnly = true)
     public RecordDetailsResponseDto getReviewsDetails(String isbn13) {
         Member member = memberService.getCurrentMember();
         Book book = bookService.getOrCreateBookByIsbn(isbn13);
